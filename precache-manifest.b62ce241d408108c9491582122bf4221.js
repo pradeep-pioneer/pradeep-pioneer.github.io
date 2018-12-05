250541bcf260341c698718129e64465ca9137edc
Pradeep Singh <contact@pradeep.vip>
2018-12-06 04:41:18 +0700

    Initial commit

---
+++ precache-manifest.b62ce241d408108c9491582122bf4221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fce087f4bb3f20da3364",
+    "url": "/static/css/main.582f5406.chunk.css"
+  },
+  {
+    "revision": "fce087f4bb3f20da3364",
+    "url": "/static/js/main.fce087f4.chunk.js"
+  },
+  {
+    "revision": "9fcd1b0ff83dba862953",
+    "url": "/static/js/1.9fcd1b0f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f849c62d2dcbfa0dd40386ca914d8f1c",
+    "url": "/static/media/avatar.f849c62d.png"
+  },
+  {
+    "revision": "410dcdcde64104bf196df29adf1b49e7",
+    "url": "/index.html"
+  }
+];
